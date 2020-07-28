3aa8a8dce614d4bad518ae18319400ec0d0751f6
wilsonng21 <wilsongch92@gmail.com>
2020-07-27 20:54:54 -0400

    behaviours are as expected

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,19 @@
 # Code your solution here!
+def run_guessing_game
+    random_num = rand(6) + 1
+    puts "Random number: " + random_num.to_s
+    puts "Guess a number from 1 to 6."
+    prompt = gets.chomp
+
+    if prompt == 'exit' 
+        puts 'Goodbye!'
+
+    elsif prompt == random_num.to_s
+        puts "You guessed the correct number!"
+
+    elsif prompt != random_num
+        puts "Sorry! The computer guessed #{random_num}."
+
+    end
+
+end
